f5d1da2e565c35540326f2500eae75ee6fa52a2f
Shivam Satija <shivams@ndtv.com>
2018-01-02 12:38:04 +0530

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+JoyPlayer
+=====
+
+Joy Player is a materialized music player handcrafted by Shivam Satija and Alok Verma. The soul aim of this project is to learn RxJava, Dagger 2, Kotlin, Loaders and Services. 
---
+++ app/src/main/java/developer/shivam/joyplayer/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package developer.shivam.joyplayer.ui.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import developer.shivam.joyplayer.R;
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
